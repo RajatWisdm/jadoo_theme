4737f308fb05090e89a585afc4a57f89d32c3f87
RajatWisdm <rajat.ganguly@wisdmlabs.com>
2023-03-21 12:14:40 +0530

    Project initialized and created folder structure

---
+++ template-parts/site-header.php
@@ -0,0 +1,61 @@
+<header>
+    <img id="header-bg" src="static/Decore.png" alt="">
+    <nav>
+        <div id="nav-box">
+            <img src="static/Jadoo.png" alt="">
+            <div id="nav-items-1">
+                <a href="#">Destinations</a>
+                <a href="#">Hotels</a>
+                <a href="#">Flights</a>
+                <a href="#">Bookings</a>
+            </div>
+            <div id="nav-items-2">
+                <a href="#">Login</a>
+                <a href="#" id="signup-btn">Sign up</a>
+                <a href="#" id="en">En</a><img src="static/Vector 1.png" alt="">
+            </div>
+        </div>
+        <div class="ham-container">
+            <input type="checkbox" name="" id="" class="check">
+            <ul class="menu-items">
+                <li><a href="https://google.com" target="_blank">Destinations</a></li>
+                <li><a href="https://google.com" target="_blank">Hotels</a></li>
+                <li><a href="https://google.com" target="_blank">Flights</a></li>
+                <li><a href="https://google.com" target="_blank">Bookings</a></li>
+                <li><a href="https://google.com" target="_blank">Login</a></li>
+                <li><a href="https://google.com" target="_blank">Signup</a></li>
+            </ul>
+            <div class="ham-menu">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+        </div>
+    </nav>
+
+    <div id="poster">
+        <div id="poster-content">
+            <div id="poster-heading">
+                <h2>Best Destinations around the world</h2>
+                <h1>Travel, enjoy<span id="header-under-img"><img src="static/header/Vector.svg" alt=""></span> and live a new and full life</h1>
+            </div>
+            <p id="poster-text">Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
+            <div id="poster-btn">
+                <button id="find-out-btn">Find out more</button>
+                <div id="play">
+                    <img src="static/header/Play button.png" alt="">
+                    <span id="play-btn">Play Demo</span>
+                </div>
+            </div>
+        </div>
+        <div id="poster-img">
+            <img src="static/Traveller 1.png" alt="">
+            <div id="plain-1" class="poster-plain">
+                <img src="static/header/Group.svg" alt="">
+            </div>
+            <div id="plain-2" class="poster-plain">
+                <img src="static/header/Group.svg" alt="">
+            </div>
+        </div>
+    </div>
+</header>
